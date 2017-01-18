4e3d93c4bcc06652c9172c6689cf6f824cb18fa1
Adam Cironis <Adam Cironis>
2017-01-18 20:34:55 +0100

    Finished Project Increment 2 - Week 6

---
+++ ProjectIncrement2/GameProject/GameProject/Burger.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject
+{
+    /// <summary>
+    /// A burger
+    /// </summary>
+    public class Burger
+    {
+        #region Fields
+
+        // graphic and drawing info
+        Texture2D sprite;
+        Rectangle drawRectangle;
+
+        // burger stats
+        int health = 100;
+
+        // shooting support
+        bool canShoot = true;
+        int elapsedCooldownMilliseconds = 0;
+
+        // sound effect
+        SoundEffect shootSound;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        ///  Constructs a burger
+        /// </summary>
+        /// <param name="contentManager">the content manager for loading content</param>
+        /// <param name="spriteName">the sprite name</param>
+        /// <param name="x">the x location of the center of the burger</param>
+        /// <param name="y">the y location of the center of the burger</param>
+        /// <param name="shootSound">the sound the burger plays when shooting</param>
+        public Burger(ContentManager contentManager, string spriteName, int x, int y,
+            SoundEffect shootSound)
+        {
+            LoadContent(contentManager, spriteName, x, y);
+            this.shootSound = shootSound;
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the collision rectangle for the burger
+        /// </summary>
+        public Rectangle CollisionRectangle
+        {
+            get { return drawRectangle; }
+        }
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Updates the burger's location based on mouse. Also fires 
+        /// french fries as appropriate
+        /// </summary>
+        /// <param name="gameTime">game time</param>
+        /// <param name="mouse">the current state of the mouse</param>
+        public void Update(GameTime gameTime, MouseState mouse)
+        {
+            // burger should only respond to input if it still has health
+            if (health > 0)
+            {
+                // move burger using mouse
+                drawRectangle.X = mouse.X;
+                drawRectangle.Y = mouse.Y;
+
+                // clamp burger in window
+                if (drawRectangle.Right > GameConstants.WindowWidth)
+                {
+                    drawRectangle.X = GameConstants.WindowWidth - drawRectangle.Width;
+                }
+
+                if (drawRectangle.Left < 0)
+                {
+                    drawRectangle.X = 0;
+                }
+
+                if (drawRectangle.Bottom > GameConstants.WindowHeight)
+                {
+                    drawRectangle.Y = GameConstants.WindowHeight - drawRectangle.Height;
+                }
+
+                if (drawRectangle.Y < 0)
+                {
+                    drawRectangle.Y = 0; 
+                }
+
+                // update shooting allowed
+                if (mouse.LeftButton == ButtonState.Pressed && canShoot == true)
+                {
+                    canShoot = false;
+
+                    // Setting projectile type
+                    ProjectileType type = ProjectileType.FrenchFries;
+                    Texture2D frenchfrySprite = Game1.GetProjectileSprite(type);
+                    int x = drawRectangle.Center.X;
+                    int y = drawRectangle.Center.Y + GameConstants.FrenchFriesProjectileOffset;
+
+                    // Creating new projectile
+                    Projectile projectile = new Projectile(type, frenchfrySprite, x, y, 
+                                            GameConstants.FrenchFriesProjectileSpeed);
+
+                    // Adding projectile to Game1
+                    Game1.AddProjectile(projectile);
+                }
+                // timer concept (for animations) introduced in Chapter 7
+
+                // shoot if appropriate
+                if (canShoot == false)
+                {
+                    elapsedCooldownMilliseconds += gameTime.ElapsedGameTime.Milliseconds;
+                    if (elapsedCooldownMilliseconds >= GameConstants.BurgerTotalCooldownMilliseconds ||
+                                                      mouse.LeftButton == ButtonState.Released)
+                    {
+                        // Resetting shooting
+                        elapsedCooldownMilliseconds = 0;
+                        canShoot = true;
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Draws the burger
+        /// </summary>
+        /// <param name="spriteBatch">the sprite batch to use</param>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the burger
+            spriteBatch.Draw(sprite, drawRectangle, Color.White);
+        }
+
+        #endregion
+
+        #region Private methods
+
+        /// <summary>
+        /// Loads the content for the burger
+        /// </summary>
+        /// <param name="contentManager">the content manager to use</param>
+        /// <param name="spriteName">the name of the sprite for the burger</param>
+        /// <param name="x">the x location of the center of the burger</param>
+        /// <param name="y">the y location of the center of the burger</param>
+        private void LoadContent(ContentManager contentManager, string spriteName,
+            int x, int y)
+        {
+            // load content and set remainder of draw rectangle
+            sprite = contentManager.Load<Texture2D>(spriteName);
+            drawRectangle = new Rectangle(x - sprite.Width / 2,
+                y - sprite.Height / 2, sprite.Width,
+                sprite.Height);
+        }
+
+        #endregion
+    }
+}
---
+++ ProjectIncrement2/GameProject/GameProject/RandomNumberGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameProject
+{
+    /// <summary>
+    /// Provides a single generator for random numbers throughout the game
+    /// </summary>
+    public static class RandomNumberGenerator
+    {
+        #region Fields
+
+        static Random rand;
+
+        #endregion
+
+        #region Public methods
+
+        /// <summary>
+        /// Initializes the random number generator
+        /// </summary>
+        public static void Initialize()
+        {
+            rand = new Random();
+        }
+
+        /// <summary>
+        /// Returns a nonnegative random number less than maxValue (exclusive)
+        /// </summary>
+        /// <param name="maxValue">the exclusive max value</param>
+        /// <returns>the random number</returns>
+        public static int Next(int maxValue)
+        {
+            return rand.Next(maxValue);
+        }
+
+        /// <summary>
+        /// Returns a nonnegative random number less than maxValue (exclusive)
+        /// </summary>
+        /// <param name="maxValue">the exclusive max value</param>
+        /// <returns>the random number</returns>
+        public static float NextFloat(float maxValue)
+        {
+            return (float)rand.NextDouble() * maxValue;
+        }
+
+        /// <summary>
+        /// Returns a random number between 0.0 and 1.0
+        /// </summary>
+        /// <returns>the random number</returns>
+        public static double NextDouble()
+        {
+            return rand.NextDouble();
+        }
+
+        #endregion
+    }
+}
